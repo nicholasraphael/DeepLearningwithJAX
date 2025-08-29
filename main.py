9c5db3f676c0c5e68362e8175ccaafbf48284f98
Nicholas Raphael <nicholasraphaelgt@gmail.com>
2025-08-29 15:07:45 -0400

    cli: update main.py scaffold

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def run():
-  pass
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def inference(name: str):
+    """
+    Run inference on a given implementation.
+    """
+    print(f"Inference {name}")
+
+
+@app.command()
+def train(name: str):
+    """
+    Train a given implementation.
+    """
+    print(f"Training {name}")
+
 
 if __name__ == "__main__":
-  run()
+    app()
